ac15755fed716d53cd775bed06b93d6b439f7ebe
Paul Standley <paulstandley1972@gmail.com>
2019-05-04 11:24:03 +0100

    make warrior methods

--- Warrior.cs
+++ Warrior.cs
@@ -8,10 +8,33 @@ namespace OOPGame
 {
     class Warrior
     {
-        // name, health, attack maximum, block maximum, random numbers
+        // name, health, attack maximum, block maximum
+        public string Name { get; set; } = "Warrior";
+        public double HealthMax { get; set; } = 0;
+        public double AttackMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
+        // get random numbers, use the same instance so as not to pass the same values
+        Random GetRandom = new Random();
+        
+        public Warrior(string name = "Warrrior", double health = 0, double attackMax = 0, double block = 0)
+        {
+            Name = name;
+            HealthMax = health;
+            AttackMax = attackMax;
+            BlockMax = block;
+        }
 
         // attack generate a random number from 1 to maximum value
+        public double Attack()
+        {
+            return GetRandom.Next(1, (int)AttackMax);
+        }
 
         // block generate a random number from 1 to maximum value
+        public double Block()
+        {
+            return GetRandom.Next(1, (int)BlockMax);
+        }
+       
     }
 }
